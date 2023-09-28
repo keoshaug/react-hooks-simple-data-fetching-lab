233c34cb2f280c49f9e67a257230a192110672c6
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-09-28 10:12:23 -0400

    completing this lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+
+
+const imgAPI = 'https://images.dog.ceo/breeds/bulldog-english/mami.jpg'
+
+function App() {
+    const [image, setImage] = useState(null);
+    
+
+useEffect(()=>{
+    fetch('https://images.dog.ceo/breeds/bulldog-english/mami.jpg')
+    .then((res)=>res.json())
+    .then((data)=>setImage(data.image))
+}, [])
+
+if(!image) {
+    return <p>"Loading..."</p>
+}
+
+
+return (
+    <div>
+        <img src={imgAPI} alt="A Random Dog"/>
+    </div>
+);
+}
+
+
+export default App;
